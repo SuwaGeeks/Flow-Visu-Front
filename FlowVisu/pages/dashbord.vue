1abfa8f889298e90ccc91bc86c6573b01a4ff1c3
Sora-210 <sokominto1231@gmail.com>
2023-08-02 17:24:34 +0000

    add: ダッシュボードのベース作成

---
+++ FlowVisu/pages/dashbord.vue
@@ -0,0 +1,352 @@
+<template>
+  <div id='top'>
+    <section>
+      <div class='section-head'>
+        <h2 class='subtitle'>
+          説明ブース
+        </h2>
+      </div>
+      <div class='section-body'>
+        <ul>
+          <li>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section>
+      <div class='section-head'>
+        <h2 class='subtitle'>
+          3DCADブース
+        </h2>
+      </div>
+      <div class='section-body'>
+        <ul>
+          <li>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section>
+      <div class='section-head'>
+        <h2 class='subtitle'>
+          3DPrintブース
+        </h2>
+      </div>
+      <div class='section-body'>
+        <ul>
+          <li>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+            <div class="card">
+              <ul>
+                <li>id: -----------</li>
+                <li>担当者: xxxxx</li>
+                <li>開始時間:</li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const router = useRouter()
+
+onMounted(() => {
+  if (window.localStorage.getItem('name') !== '') {
+    router.push('/setting')
+  }
+})
+</script>
+
+<style scoped>
+#top {
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+section {
+  width: 30%;
+  padding-top: 15px;
+}
+.section-head {
+  text-align: center;
+}
+.subtitle {
+  display: inline-block;
+  border-bottom: solid 5px #7a7a7a;
+  padding: 0 20px;
+}
+.section-body {
+  padding-top: 20px;
+  overflow-y: scroll;
+  height: calc(100% - 20px - 41px);
+}
+
+.card {
+  width: 80%;
+  margin: 20px auto;
+  padding: 15px 25px;
+  border-radius: 25px;
+  border: solid 1px #392061;
+}
+
+@media screen and (max-width: 870px) {
+  #top {
+    flex-direction: column;
+    justify-content: normal;
+    overflow-y: scroll;
+  }
+  section {
+    width: 90%;
+    margin: 0 auto;
+  }
+}
+</style>
