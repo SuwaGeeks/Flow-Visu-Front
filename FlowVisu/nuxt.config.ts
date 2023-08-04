aba4d5e1dc498ebde4539973e49ea5418544a7c6
Sora-210 <sokominto1231@gmail.com>
2023-08-04 18:02:59 +0000

    update: フォントなどの変更

--- FlowVisu/nuxt.config.ts
+++ FlowVisu/nuxt.config.ts
@@ -1,6 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'FLOW-VISU',
+      meta: [
+        { charset: 'utf-8' },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.png'
+        },
+        {
+          rel: 'stylesheet',
+          href: '/Heebo.ttf'
+        }
+      ]
+    }
+  },
   plugins: [
     {
       src: '@/plugins/QrReader.ts',
