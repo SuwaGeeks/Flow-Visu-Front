653dc6676a30dc5b3e0ff7f723f03c78fdb16b7a
Sora-210 <sokominto1231@gmail.com>
2023-08-04 18:04:00 +0000

    update: 表示内容の拡充と自動更新の追加

--- FlowVisu/pages/dashbord.vue
+++ FlowVisu/pages/dashbord.vue
@@ -1,63 +1,91 @@
 <template>
   <div id='top'>
-    <section>
-      <div class='section-head'>
-        <h2 class='subtitle'>
-          説明ブース
-        </h2>
+    <section class='statistics'>
+      <div>
+        <h3>
+          ブース内人数
+        </h3>
+        <p>
+          {{ boothPeople }} 人
+        </p>
       </div>
-      <div class='section-body'>
-        <ul v-for="status in stepFilter(1)" :key='status.index'>
-          <li>
-            <div class="card">
-              <ul>
-                <li>id: {{ status.tagId }}</li>
-                <li>担当者: {{ status.operator }}</li>
-                <li>開始時間: {{ status.startTime }}</li>
-              </ul>
-            </div>
-          </li>
-        </ul>
+      <div>
+        <h3>
+          体験者総数
+        </h3>
+        <p>
+          {{ statusList.length }} 人
+        </p>
       </div>
-    </section>
-    <section>
-      <div class='section-head'>
-        <h2 class='subtitle'>
-          3DCADブース
-        </h2>
-      </div>
-      <div class='section-body'>
-        <ul v-for="status in stepFilter(2)" :key='status.index'>
-          <li>
-            <div class="card">
-              <ul>
-                <li>id: {{ status.tagId }}</li>
-                <li>担当者: {{ status.operator }}</li>
-                <li>開始時間: {{ status.startTime }}</li>
-              </ul>
-            </div>
-          </li>
-        </ul>
+      <div>
+        <h3>
+          最終更新時間
+        </h3>
+        <p>
+          {{ convertTime(lastUpdate) }}
+        </p>
       </div>
     </section>
-    <section>
-      <div class='section-head'>
-        <h2 class='subtitle'>
-          3DPrintブース
-        </h2>
+    <section class='dashboard'>
+      <div class='status'>
+        <div class='section-head'>
+          <h2 class='subtitle'>
+            説明ブース
+          </h2>
+        </div>
+        <div class='section-body'>
+          <ul v-for="status in stepFilter(1)" :key='status.index'>
+            <li>
+              <div class="card">
+                <ul>
+                  <li>id: {{ status.tagId }}</li>
+                  <li>担当者: {{ status.operator }}</li>
+                  <li>開始時間: {{ convertTime(status.created_at) }}</li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class='section-body'>
-        <ul v-for="status in stepFilter(3)" :key='status.index'>
-          <li>
-            <div class="card">
-              <ul>
-                <li>id: {{ status.tagId }}</li>
-                <li>担当者: {{ status.operator }}</li>
-                <li>開始時間: {{ status.startTime }}</li>
-              </ul>
-            </div>
-          </li>
-        </ul>
+      <div class='status'>
+        <div class='section-head'>
+          <h2 class='subtitle'>
+            3DCADブース
+          </h2>
+        </div>
+        <div class='section-body'>
+          <ul v-for="status in stepFilter(2)" :key='status.index'>
+            <li>
+              <div class="card">
+                <ul>
+                  <li>id: {{ status.tagId }}</li>
+                  <li>担当者: {{ status.operator }}</li>
+                  <li>開始時間: {{ convertTime(status.created_at) }}</li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class='status'>
+        <div class='section-head'>
+          <h2 class='subtitle'>
+            3DPrintブース
+          </h2>
+        </div>
+        <div class='section-body'>
+          <ul v-for="status in stepFilter(3)" :key='status.index'>
+            <li>
+              <div class="card">
+                <ul>
+                  <li>id: {{ status.tagId }}</li>
+                  <li>担当者: {{ status.operator }}</li>
+                  <li>開始時間: {{ convertTime(status.created_at) }}</li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   </div>
@@ -77,26 +105,33 @@ const statusList = ref<Array<{
   "stepNo": Number
   "content": String
 }>>([])
+const lastUpdate = ref(0)
 
 onMounted(async () => {
   if (!window.localStorage.getItem('name')) {
     router.push('/setting')
     return
   }
+  await update()
+  setInterval(async() => {
+    await update()
+  }, 30000)
+})
 
+const update = async () => {
   try {
-    const res = await fetch('https://api.flow-visu.suwageeks.org' + '/status', {
+    const res = await fetch('https://api.flow-visu.suwageeks.org/status', {
       method: "get"
     })
     statusList.value = JSON.parse(await res.text()).data
+    lastUpdate.value = new Date()
   } catch(e) {
     alert('APIの情報が取得できません')
     router.push('/')
   }
-})
+}
 
 const stepFilter = (stepNo: Number) => {
-  console.log(statusList.value)
   if (statusList.value) {
     return statusList.value.filter((status) => {
       return status.stepNo === stepNo
@@ -105,17 +140,46 @@ const stepFilter = (stepNo: Number) => {
     return []
   }
 }
+
+const boothPeople = computed(() => {
+  if (statusList.value) {
+    return statusList.value.filter((status) => {
+      return status.stepNo < 4
+    }).length
+  } else {
+    return 0
+  }
+})
+
+const convertTime = (d) => {
+  return (new Date(d)).toLocaleString('jp-ja') 
+}
 </script>
 
 <style scoped>
 #top {
   height: 100%;
+}
+
+.statistics {
+  height: 10%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.statistics p {
+  text-align: center;
+}
+
+.dashboard {
+  height: 90%;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+  border-top: solid 3px #00BD9D;
 }
 
-section {
+.status {
   width: 30%;
   padding-top: 15px;
 }
@@ -142,12 +206,19 @@ section {
 }
 
 @media screen and (max-width: 870px) {
-  #top {
+  .statistics,
+  .dashboard {
     flex-direction: column;
     justify-content: normal;
     overflow-y: scroll;
+    height: auto;
   }
-  section {
+
+  .statistics div { 
+    margin: 10px 0;
+  }
+
+  .status {
     width: 90%;
     margin: 0 auto;
   }
